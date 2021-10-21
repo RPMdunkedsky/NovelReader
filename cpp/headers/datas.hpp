3f0488e536ec07df74ba799bf910343af4144920
RPMdunkedsky <79433973+RPMdunkedsky@users.noreply.github.com>
2021-10-21 22:02:23 +0800

    New Objects.

--- cpp/headers/datas.hpp
+++ cpp/headers/datas.hpp
@@ -1,15 +1,49 @@
 #include<bits/stdc++.h>
+#include"const.hpp"
 using namespace std;
 #define GITHUB
+
 struct chapter{
     string name;
-    vector<string> chap;
+    vector<string> data;
+    void output(){
+        cout<<"\t\t"<<name<<endl;
+        for(int i=0;i<vector.size();i++){
+            cout<<data[i]<<"\n";
+        }
+        char ch;
+        cout<<"Input Enter To Return:";
+        cin>>ch;
+        system("cls");
+    }
 };
 struct book{
     string name,author;
     vector<chapter> chapters;
 };
 class reader{
-    string name;
-    vector<book> bookshelf;
-};
+    private:
+        string name;
+        vector<book> bookshelf;
+    public:
+        void NewReader(){
+            reader r;
+            cout<<"Input Name:";
+            cin>>r.name;
+            this=r;
+        }
+        void ChangeUserName(){
+            cout<<"Old Name:"<<this.name;
+            string newname;
+            cout<<"Input New Name:";
+            cin>>r;
+            this.name=r;
+            system("cls");
+            cout<<"New Name:"<<this.name;
+            cout<<"Successfully Changed name!";
+            return;
+        }
+        void readNewBook(book k){
+            cout<<
+        }
+}
