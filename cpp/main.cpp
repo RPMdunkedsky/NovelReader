4de03731b21eeb57abb9d1219038a32ff2aab1c7
RPMdunkedsky <79433973+RPMdunkedsky@users.noreply.github.com>
2021-10-30 11:36:29 +0800

    Create main.cpp

---
+++ cpp/main.cpp
@@ -0,0 +1,10 @@
+#include"write.hpp"
+#include"const.hpp"
+#include"main.hpp"
+#include"datas.hpp"
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"Waiting update for books."<<"\n";
+    return 0;
+}
